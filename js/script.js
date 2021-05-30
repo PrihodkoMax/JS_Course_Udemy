22233e0fc5e3d4396ab3bb8ce4b87ea4b6f61377
PrihodkoMax <1986maxim1508@gmail.com>
2021-05-30 17:30:52 +0300

    Lesson 12 first practical task

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
 "use strict";
 
-let incr = 10,
-    decr = 10;
+const numberOfFilms = +prompt('Сколько фильмов Вы уже посморели?', '');
 
-// ++incr;
-// --decr;
-console.log(incr++);	 
-console.log(decr--);	 
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false,
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?',''),
+		c = prompt('Один из последних просмотренных фильмов?', ''),
+		d = prompt('На сколько оцените его?','');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
