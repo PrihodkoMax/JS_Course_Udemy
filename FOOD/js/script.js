aed844ce17e9a63a3f6f5e28c418ea6bdb680cf7
Maxim <1986maxim1508@gmail.com>
2021-10-06 13:54:59 +0300

    Add tabs toggling to the page

--- FOOD/js/script.js
+++ FOOD/js/script.js
@@ -3,7 +3,46 @@
 
 window.addEventListener('DOMContentLoaded', () => {
 
+	// Tabs
 
+	const tabsContent = document.querySelectorAll('.tabcontent'),
+		tabsParent = document.querySelector('.tabheader__items'),
+		tabs = document.querySelectorAll('.tabheader__item');
+
+
+	function hideTabsContent() {
+		tabsContent.forEach(item => {
+			item.classList.add('hide');
+			item.classList.remove('show', 'fade');
+		});
+
+		tabs.forEach(tab => {
+			tab.classList.remove('tabheader__item_active');
+		});
+	}
+
+	function showTabContent(i = 0) {
+		tabsContent[i].classList.add('show', 'fade');
+		tabsContent[i].classList.remove('hide');
+		tabs[i].classList.add('tabheader__item_active');
+	}
+
+	hideTabsContent(); // Скрываем все табы на странице
+
+	showTabContent();  // Показываем только первый таб по дэфолту
+
+	tabsParent.addEventListener('click', (event) => {
+		const target = event.target;
+
+		if (target && target.classList.contains('tabheader__item')) {
+			tabs.forEach((tab, i) => {
+				if (target == tab) {
+					hideTabsContent();
+					showTabContent(i);
+				}
+			});
+		}
+	});
 
 });
 
