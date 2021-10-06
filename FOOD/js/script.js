be4a3543221acd2962e8473777dd61722ad9f8b0
Maxim <1986maxim1508@gmail.com>
2021-10-06 17:35:41 +0300

    Add Timer

--- FOOD/js/script.js
+++ FOOD/js/script.js
@@ -44,6 +44,59 @@ window.addEventListener('DOMContentLoaded', () => {
 		}
 	});
 
+	// Timer 
+
+	const deadLine = '2021-10-30';
+
+	function getRemainingDate(overDate) {
+		const t = Date.parse(overDate) - Date.parse(new Date()),
+			d = Math.floor(t / (1000 * 60 * 60 * 24)),
+			h = Math.floor(t / (1000 * 60 * 60) % 24),
+			m = Math.floor(t / (1000 * 60) % 60),
+			s = Math.floor(t / 1000 % 60);
+
+		return {
+			'total': t,
+			'days': d,
+			'hours': h,
+			'minutes': m,
+			'seconds': s,
+		};
+	}
+
+	function setZero(num) {
+		if (num >= 0 && num < 10) {
+			return `0${num}`;
+		} else {
+			return num;
+		}
+	}
+
+	function setCounter(selector, endTime) {
+		let timer = document.querySelector(selector),
+			days = timer.querySelector('#days'),
+			hours = timer.querySelector('#hours'),
+			minutes = timer.querySelector('#minutes'),
+			seconds = timer.querySelector('#seconds'),
+			timerId = setInterval(updateClock, 1000);
+
+		updateClock();
+
+		function updateClock() {
+			const t = getRemainingDate(endTime);
+			days.innerHTML = setZero(t.days);
+			hours.innerHTML = setZero(t.hours);
+			minutes.innerHTML = setZero(t.minutes);
+			seconds.innerHTML = setZero(t.seconds);
+
+			if (t.total <= 0) {
+				clearInterval(timerId);
+			}
+		}
+	}
+
+	setCounter('.timer', deadLine);
+
 });
 
 
